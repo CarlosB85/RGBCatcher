dc24a64eb9d04242409ca3db807a1a030b794804
Carlos Brugnoni <carlos.brugnoni@gmail.com>
2018-10-29 16:24:42 -0500

    Inicial files no MVS project

---
+++ Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace RGBCatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_probar = new System.Windows.Forms.Button();
+            this.pnl_rgb_color = new System.Windows.Forms.Panel();
+            this.lbl_ancho = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_alto = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inpt_delay = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lst_puntos = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chk_preview = new System.Windows.Forms.CheckBox();
+            this.chk_modo2 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inpt_px_ancho = new System.Windows.Forms.NumericUpDown();
+            this.inpt_px_largo = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_delay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_px_ancho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_px_largo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_probar
+            // 
+            this.btn_probar.Location = new System.Drawing.Point(452, 390);
+            this.btn_probar.Name = "btn_probar";
+            this.btn_probar.Size = new System.Drawing.Size(75, 23);
+            this.btn_probar.TabIndex = 0;
+            this.btn_probar.Text = "Probar";
+            this.btn_probar.UseVisualStyleBackColor = true;
+            this.btn_probar.Click += new System.EventHandler(this.btn_probar_Click);
+            // 
+            // pnl_rgb_color
+            // 
+            this.pnl_rgb_color.Location = new System.Drawing.Point(349, 392);
+            this.pnl_rgb_color.Name = "pnl_rgb_color";
+            this.pnl_rgb_color.Size = new System.Drawing.Size(20, 20);
+            this.pnl_rgb_color.TabIndex = 2;
+            // 
+            // lbl_ancho
+            // 
+            this.lbl_ancho.AutoSize = true;
+            this.lbl_ancho.Location = new System.Drawing.Point(50, 9);
+            this.lbl_ancho.Name = "lbl_ancho";
+            this.lbl_ancho.Size = new System.Drawing.Size(38, 13);
+            this.lbl_ancho.TabIndex = 3;
+            this.lbl_ancho.Text = "Ancho";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ancho:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Alto:";
+            // 
+            // lbl_alto
+            // 
+            this.lbl_alto.AutoSize = true;
+            this.lbl_alto.Location = new System.Drawing.Point(50, 31);
+            this.lbl_alto.Name = "lbl_alto";
+            this.lbl_alto.Size = new System.Drawing.Size(25, 13);
+            this.lbl_alto.TabIndex = 6;
+            this.lbl_alto.Text = "Alto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "delay";
+            // 
+            // inpt_delay
+            // 
+            this.inpt_delay.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.inpt_delay.Location = new System.Drawing.Point(189, 7);
+            this.inpt_delay.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.inpt_delay.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.inpt_delay.Name = "inpt_delay";
+            this.inpt_delay.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.inpt_delay.Size = new System.Drawing.Size(60, 20);
+            this.inpt_delay.TabIndex = 1;
+            this.inpt_delay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inpt_delay.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ms";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(452, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(452, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Status";
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(50, 53);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(37, 13);
+            this.lbl_status.TabIndex = 13;
+            this.lbl_status.Text = "Status";
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(452, 84);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_registrar.TabIndex = 14;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            this.btn_registrar.Click += new System.EventHandler(this.btn_registrar_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lst_puntos
+            // 
+            this.lst_puntos.FormattingEnabled = true;
+            this.lst_puntos.Location = new System.Drawing.Point(12, 84);
+            this.lst_puntos.Name = "lst_puntos";
+            this.lst_puntos.Size = new System.Drawing.Size(434, 329);
+            this.lst_puntos.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(452, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // chk_preview
+            // 
+            this.chk_preview.AutoSize = true;
+            this.chk_preview.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chk_preview.Location = new System.Drawing.Point(27, 419);
+            this.chk_preview.Name = "chk_preview";
+            this.chk_preview.Size = new System.Drawing.Size(81, 17);
+            this.chk_preview.TabIndex = 17;
+            this.chk_preview.Text = "Vista previa";
+            this.chk_preview.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_preview.UseVisualStyleBackColor = true;
+            // 
+            // chk_modo2
+            // 
+            this.chk_modo2.AutoSize = true;
+            this.chk_modo2.Location = new System.Drawing.Point(114, 419);
+            this.chk_modo2.Name = "chk_modo2";
+            this.chk_modo2.Size = new System.Drawing.Size(62, 17);
+            this.chk_modo2.TabIndex = 18;
+            this.chk_modo2.Text = "Modo 2";
+            this.chk_modo2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(129, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "px ancho";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(129, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "px largo";
+            // 
+            // inpt_px_ancho
+            // 
+            this.inpt_px_ancho.Location = new System.Drawing.Point(189, 29);
+            this.inpt_px_ancho.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.inpt_px_ancho.Name = "inpt_px_ancho";
+            this.inpt_px_ancho.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.inpt_px_ancho.Size = new System.Drawing.Size(60, 20);
+            this.inpt_px_ancho.TabIndex = 21;
+            this.inpt_px_ancho.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inpt_px_ancho.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // inpt_px_largo
+            // 
+            this.inpt_px_largo.Location = new System.Drawing.Point(189, 51);
+            this.inpt_px_largo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.inpt_px_largo.Name = "inpt_px_largo";
+            this.inpt_px_largo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.inpt_px_largo.Size = new System.Drawing.Size(60, 20);
+            this.inpt_px_largo.TabIndex = 22;
+            this.inpt_px_largo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inpt_px_largo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(255, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "px";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(255, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "px";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 473);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.inpt_px_largo);
+            this.Controls.Add(this.inpt_px_ancho);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chk_modo2);
+            this.Controls.Add(this.chk_preview);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lst_puntos);
+            this.Controls.Add(this.btn_registrar);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inpt_delay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_alto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_ancho);
+            this.Controls.Add(this.pnl_rgb_color);
+            this.Controls.Add(this.btn_probar);
+            this.Name = "Form1";
+            this.Text = "RGBCatcher";
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_delay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_px_ancho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpt_px_largo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_probar;
+        private System.Windows.Forms.Panel pnl_rgb_color;
+        private System.Windows.Forms.Label lbl_ancho;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_alto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown inpt_delay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ListBox lst_puntos;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox chk_preview;
+        private System.Windows.Forms.CheckBox chk_modo2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown inpt_px_ancho;
+        private System.Windows.Forms.NumericUpDown inpt_px_largo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGBCatcher
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        //List<Point> posiciones = new List<Point>();
+        int segundosTimer2 = 3;
+        int ancho_px = 1;
+        int alto_px = 1;
+        int temp_r = 0;
+        int temp_g = 0;
+        int temp_b = 0;
+        int temp_a = 0;
+        int temp_t = 0;
+        Color temp_c;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1 = new Timer();
+            timer1.Tick += timer1_Tick;
+            timer1 = new Timer();
+            timer1.Tick += timer1_Tick;
+            lst_puntos.DrawMode = DrawMode.OwnerDrawFixed;
+            lst_puntos.DrawItem += listBox_DrawItem;
+        }
+
+        private void btn_probar_Click(object sender, EventArgs e)
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int height = Screen.PrimaryScreen.Bounds.Height;
+
+            lbl_ancho.Text = width + " px";
+            lbl_alto.Text = height + " px";
+        }
+
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, ancho_px, alto_px, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                     gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            IntPtr desktopPtr = GetDC(IntPtr.Zero);
+            Graphics g = Graphics.FromHdc(desktopPtr);
+            g.DrawRectangle(new Pen(Color.Orange), new Rectangle(location.X, location.Y, ancho_px, alto_px));
+            g.Dispose();
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+
+            temp_r = 0;
+            temp_g = 0;
+            temp_b = 0;
+            temp_a = 0;
+
+            for (int px_x = 0; px_x < ancho_px; px_x++)
+            {
+                for (int px_y = 0; px_y < alto_px; px_y++)
+                {
+                    temp_c = screenPixel.GetPixel(px_x, px_y);
+                    temp_r += temp_c.R;
+                    temp_g += temp_c.G;
+                    temp_b += temp_c.B;
+                    temp_a += temp_c.A;
+                }
+            }
+            temp_t = (ancho_px * alto_px);
+            return Color.FromArgb(temp_a / temp_t, temp_r / temp_t, temp_g / temp_t, temp_b / temp_t);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            foreach (RGBPoint pnt in lst_puntos.Items)
+            {
+                var c = GetColorAt(pnt.getpnt());
+                pnt.setc(c);
+
+            }
+            lbl_status.Text = timer1.Enabled ? "Corriendo" : "Detenido";
+            lst_puntos.Refresh();
+        }
+
+        //iniciar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ancho_px = (int)inpt_px_ancho.Value;
+            alto_px = (int)inpt_px_largo.Value;
+            screenPixel = new Bitmap(ancho_px, alto_px, PixelFormat.Format32bppArgb);
+            if (!timer1.Enabled)
+            {
+                timer1.Interval = (int)inpt_delay.Value;
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            lbl_status.Text = timer1.Enabled ? "Corriendo" : "Detenido";
+        }
+
+        //detener
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            }
+            lbl_status.Text = timer1.Enabled ? "Corriendo" : "Detenido";
+        }
+
+        private void btn_registrar_Click(object sender, EventArgs e)
+        {
+            segundosTimer2 = 2;
+            timer2.Interval = 1000;
+            timer2.Enabled = true;
+            timer2.Start();
+            lbl_status.Text = "Capturando 2 s";
+        }
+
+        private void addPunto(Point pnt)
+        {
+            lst_puntos.Items.Add(new RGBPoint(pnt, Color.White));
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            segundosTimer2--;
+            lbl_status.Text = "Capturando " + segundosTimer2 + " s";
+            if (segundosTimer2 == 0)
+            {
+                timer2.Enabled = false;
+                timer2.Stop();
+                Point cursor = new Point();
+                GetCursorPos(ref cursor);
+                addPunto(cursor);
+                lbl_status.Text = "listo.";
+            }
+        }
+
+        private void listBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+
+            if (chk_preview.Checked)
+            {
+                if (chk_modo2.Checked)
+                {
+                    e.DrawBackground();
+                    ListBox lb = (ListBox)sender;
+                    RGBPoint pnt = (RGBPoint)lb.Items[e.Index];
+                    Color cc = pnt.getc();
+
+                    Graphics g = e.Graphics;
+                    g.FillRectangle(new SolidBrush(Color.White), e.Bounds);
+
+                    SizeF ss = g.MeasureString(pnt.ToString(), e.Font);
+                    g.DrawString(pnt.ToString(), e.Font, new SolidBrush(Color.Black), new PointF(e.Bounds.X, e.Bounds.Y));
+                    g.FillRectangle(new SolidBrush(cc), e.Bounds.X + ss.Width + 5, e.Bounds.Y, 18, e.Bounds.Height);
+                    g.DrawString("R (" + cc.R + ")", e.Font, new SolidBrush(Color.FromArgb(cc.R, 0, 0)), new PointF(e.Bounds.X + ss.Width + 25, e.Bounds.Y));
+                    g.DrawString("G (" + cc.G + ")", e.Font, new SolidBrush(Color.FromArgb(0, cc.G, 0)), new PointF(e.Bounds.X + ss.Width + 65, e.Bounds.Y));
+                    g.DrawString("B (" + cc.B + ")", e.Font, new SolidBrush(Color.FromArgb(0, 0, cc.B)), new PointF(e.Bounds.X + ss.Width + 105, e.Bounds.Y));
+                    g.DrawString("A (" + cc.B + ")", e.Font, new SolidBrush(Color.Black), new PointF(e.Bounds.X + ss.Width + 145, e.Bounds.Y));
+                    e.DrawFocusRectangle();
+                }
+                else
+                {
+                    e.DrawBackground();
+                    ListBox lb = (ListBox)sender;
+                    RGBPoint pnt = (RGBPoint)lb.Items[e.Index];
+                    Color cc = pnt.getc();
+
+                    Graphics g = e.Graphics;
+                    g.FillRectangle(new SolidBrush(cc), e.Bounds);
+
+                    if ((cc.R + cc.G + cc.B) > 186)
+                    {
+                        cc = Color.Black;
+                    }
+                    else
+                    {
+                        cc = Color.White;
+                    }
+
+                    g.DrawString(pnt.ToString(), e.Font, new SolidBrush(cc), new PointF(e.Bounds.X, e.Bounds.Y));
+                    e.DrawFocusRectangle();
+                }
+            }
+            else
+            {
+                e.DrawBackground();
+                e.Graphics.FillRectangle(new SolidBrush(Color.White), e.Bounds);
+                e.Graphics.DrawString(((RGBPoint)((ListBox)sender).Items[e.Index]).ToString(), e.Font, new SolidBrush(Color.Black), new PointF(e.Bounds.X, e.Bounds.Y));
+                e.DrawFocusRectangle();
+            }
+
+        }
+
+    }
+
+    public class RGBPoint
+    {
+        private Point pnt;
+        private Color c;
+
+        public RGBPoint(Point pnt, Color c)
+        {
+            this.pnt = pnt;
+            this.c = c;
+        }
+
+        public void setc(Color c) { this.c = c; }
+
+        public Point getpnt() { return this.pnt; }
+
+        public Color getc() { return this.c; }
+
+        public string ToString()
+        {
+            return "X: " + pnt.X + " - Y: " + pnt.Y;
+        }
+    }
+}
